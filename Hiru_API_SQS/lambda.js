94123b486899d7b603a6d29ba66207badf3cc781
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-06 16:29:13 +0530

    initial commit

---
+++ Hiru_API_SQS/lambda.js
@@ -0,0 +1,33 @@
+let AWS = require('aws-sdk');
+let SL = require('@slappforge/slappforge-sdk');
+const sqs = new SL.AWS.SQS(AWS);
+exports.handler = function (event, context, callback) {
+	sqs.sendMessage({
+		MessageBody: 'This is a test message',
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/480964559519/Hiru_SQS',
+		DelaySeconds: '0',
+		MessageAttributes: {
+			"name": {
+				"DataType": "String",
+				"StringValue": "001"
+			},
+			"number": {
+				"DataType": "Number",
+				"StringValue": "001"
+			},
+			"Binary": {
+				"DataType": "Binary",
+				"BinaryValue": "01"
+			}
+		}
+	}, function (data) {
+		// your logic (logging etc) to handle successful message delivery, should be here
+		console.log('data',data);
+	}, function (error) {
+		// your logic (logging etc) to handle failures, should be here
+		console.log('error' ,error);
+	});
+
+
+	callback(null, 'Successfully executed');
+}
